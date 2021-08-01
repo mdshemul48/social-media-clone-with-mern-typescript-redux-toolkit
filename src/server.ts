c9239ec8201642754de4fee9650e193091825bf1
MD Shimul <mdshemul480@gmail.com>
2021-08-02 01:27:34 +0600

    settting nodemon with project

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+// server config
+dotenv.config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hell world");
+});
+
+app.listen(port, () => {
+  console.log("api rocks in http://localhost:5000");
+});
