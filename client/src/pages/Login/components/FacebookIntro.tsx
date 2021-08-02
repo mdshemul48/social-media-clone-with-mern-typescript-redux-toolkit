87861c49862fd6fabf18ca8694ba514e5106d64b
MD Shimul <mdshemul480@gmail.com>
2021-08-02 18:00:04 +0600

    moving folder under page

--- client/src/pages/Login/components/FacebookIntro.tsx
+++ client/src/pages/Login/components/FacebookIntro.tsx
@@ -1,4 +1,4 @@
-import facebookLogo from '../../assets/nameLogo.svg';
+import facebookLogo from '../../../assets/nameLogo.svg';
 const FacebookIntro = () => {
   return (
     <div className="login__info w-75 mx-auto mt-lg-5">
--- client/src/pages/Login/components/SignupModel.tsx
+++ client/src/pages/Login/components/SignupModel.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Modal } from 'react-bootstrap';
 // interface (type)
-import { signupModel } from '../../types/login';
+import { signupModel } from '../../../types/login';
 
 // component
 import SignupForm from './SignupForm';
