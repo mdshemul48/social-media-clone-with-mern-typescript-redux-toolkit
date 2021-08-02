f6593f0c23e6b7252e0a0d6bcb897404d5afe8b9
MD Shimul <mdshemul480@gmail.com>
2021-08-02 11:31:14 +0600

    adding bootstrap in frontend

--- client/src/App.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function App() {
-    return (<div className="App">
-      <h1>hello world</h1>
-    </div>);
-}
-exports.default = App;
