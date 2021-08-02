a93e9e1b2a52a0431ffcbde3dd7b29300219c3ec
MD Shimul <mdshemul480@gmail.com>
2021-08-02 11:26:30 +0600

    fixing small error

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function App() {
+    return (<div className="App">
+      <h1>hello world</h1>
+    </div>);
+}
+exports.default = App;
