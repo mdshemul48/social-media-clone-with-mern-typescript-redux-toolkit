a8cfd0b6bf549e3e300cda7b0566627f97600d92
MD Shimul <mdshemul480@gmail.com>
2021-08-03 13:26:19 +0600

    exporting type of dispath from reducer index

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,4 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userState from './reducers/user';
 
-export default configureStore({ reducer: { userState } });
+const store = configureStore({ reducer: { userState } });
+
+// dispatch type
+export type AppDispatch = typeof store.dispatch;
+
+// state type
+export type RootState = ReturnType<AppDispatch>;
+
+export default store;
