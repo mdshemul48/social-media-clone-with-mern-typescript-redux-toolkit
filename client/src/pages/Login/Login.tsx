39171f511e6b840285b79b8c1668a7c25f5070df
MD Shimul <mdshemul480@gmail.com>
2021-08-03 15:59:38 +0600

    adding error message at login page

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,20 +1,40 @@
-import { Row, Col, Container } from 'react-bootstrap';
+import { useEffect } from 'react';
 
+import { Toaster, toast } from 'react-hot-toast';
+import { Row, Col, Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+
+// type
+import { UserReducer } from '../../types/userReducer';
 // components
 import FacebookIntro from './components/FacebookIntro';
 import LoginSection from './components/LoginSection';
 const Login = () => {
+  const { errors } = useSelector(
+    (state: { userState: UserReducer }) => state.userState
+  );
+
+  useEffect(() => {
+    if (errors.length > 0) {
+      errors.forEach((error) => toast.error(error.msg));
+    }
+  }, [errors]);
+
   return (
-    <Container className="mt-lg-5">
-      <Row>
-        <Col lg={6} md={6} sm={12}>
-          <FacebookIntro />
-        </Col>
-        <Col lg={6} md={6} sm={12}>
-          <LoginSection />
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+
+      <Container className="mt-lg-5">
+        <Row>
+          <Col lg={6} md={6} sm={12}>
+            <FacebookIntro />
+          </Col>
+          <Col lg={6} md={6} sm={12}>
+            <LoginSection />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
