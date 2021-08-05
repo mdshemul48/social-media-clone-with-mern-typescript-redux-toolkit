116624753cb87fe2ed3999edc11084121529c4d9
MD Shimul <mdshemul480@gmail.com>
2021-08-05 13:14:24 +0600

    adding input handler

--- client/src/pages/Login/components/LoginForm.tsx
+++ client/src/pages/Login/components/LoginForm.tsx
@@ -1,16 +1,34 @@
+import { useState, ChangeEvent } from 'react';
 import { Form, Button } from 'react-bootstrap';
+
 const LoginForm = () => {
+  const [formState, setFormState] = useState({
+    email: '',
+    password: ''
+  });
+
+  const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormState((PrevState) => ({ ...PrevState, [name]: value }));
+  };
+
   return (
     <Form>
       <Form.Group className="mb-2">
         <Form.Control
+          onChange={inputChangeHandler}
           placeholder="Email Address"
           type="email"
           className="py-2"
         />
       </Form.Group>
       <Form.Group className="mb-2">
-        <Form.Control placeholder="Password" type="password" className="py-2" />
+        <Form.Control
+          onChange={inputChangeHandler}
+          placeholder="Password"
+          type="password"
+          className="py-2"
+        />
       </Form.Group>
       <Form.Group className="d-grid mb-2">
         <Button variant="primary" size="lg">
