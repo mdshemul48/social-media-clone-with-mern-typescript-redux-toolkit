73d148705bc82507af6172d52afb691dab81ee6c
MD Shimul <mdshemul480@gmail.com>
2021-08-06 15:13:30 +0600

    adding user check in nav bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 
+import { useSelector } from 'react-redux';
+import { UserReducer } from './types/userReducer';
+
 // routes
 import PrivateRoute from './Routes/PrivateRoute';
 import LoginRoute from './Routes/LoginRoute';
@@ -10,9 +13,13 @@ import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 
 function App() {
+  const { user } = useSelector(
+    (state: { userState: UserReducer }) => state.userState
+  );
+
   return (
     <Router>
-      <NavBar />
+      {user && <NavBar />}
       <Switch>
         <LoginRoute path="/login" component={Login} />
         <PrivateRoute path="/" component={Home} />
