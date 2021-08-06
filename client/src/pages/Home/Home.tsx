3a5f555af51089e8a624308e6c6f28ff64bbe764
MD Shimul <mdshemul480@gmail.com>
2021-08-06 14:48:04 +0600

    creating homepage

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Home = () => {
-    return (
-        <div>
-            <h1>this is home</h1>
-        </div>
-    )
-}
+  return (
+    <Container fluid>
+      <Row>
+        <Col lg={2}>this is home</Col>
+        <Col lg={8}>this is home</Col>
+        <Col lg={2}>this is home</Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
