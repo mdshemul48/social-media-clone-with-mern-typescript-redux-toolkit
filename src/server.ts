907410e6396a99b828983e47c05a1e05a66d1fc1
MD Shimul <mdshemul480@gmail.com>
2021-08-07 17:36:02 +0600

    adding static folder for images

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import cors from 'cors';
 // mongoose connector function
 import mongooseConnect from './config/mongooseConnect';
@@ -12,7 +13,8 @@ const port = process.env.PORT || 5000;
 const server = express();
 // connecting mongoose
 mongooseConnect();
-
+console.log(__dirname);
+server.use('/public', express.static(path.join(__dirname, '../images')));
 server.use(cors());
 server.use(express.json());
 
