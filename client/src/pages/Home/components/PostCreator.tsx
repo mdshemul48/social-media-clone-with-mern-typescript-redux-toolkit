cb42478170f1e60f2bfd4980ae0e95ba666b9d7f
MD Shimul <mdshemul480@gmail.com>
2021-08-07 13:24:30 +0600

    adding newsfeed and post creator area

---
+++ client/src/pages/Home/components/PostCreator.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+const PostCreator = () => {
+  return <Container>post Creator</Container>;
+};
+
+export default PostCreator;
