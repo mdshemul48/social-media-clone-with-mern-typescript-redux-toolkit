cb6785e7af2cd64d112cfc3fba92c4521d1fb050
MD Shimul <mdshemul480@gmail.com>
2021-08-09 17:42:47 +0600

    adding post button for opening the modal

--- client/src/pages/Home/components/PostCreator.tsx
+++ client/src/pages/Home/components/PostCreator.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+
+import { UserReducer } from '../../../types/userReducer';
 const PostCreator = () => {
-  return <Container>post Creator</Container>;
+  const { user } = useSelector(
+    (state: { userState: UserReducer }) => state.userState
+  );
+
+  return (
+    <div className="bg-light rounded shadow-sm">
+      <div className="d-flex align-items-center p-2">
+        {user && (
+          <img
+            width="50px"
+            height="50px"
+            className="rounded-circle "
+            src={
+              process.env.REACT_APP_BACKEND_API_LINK +
+              `/public/${user.profileImage}`
+            }
+            alt=""
+          />
+        )}
+        <p className="flex-fill px-2 py-2 mx-2 mt-2 post_create_section">
+          what's on your mind, {user?.firstName}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default PostCreator;
