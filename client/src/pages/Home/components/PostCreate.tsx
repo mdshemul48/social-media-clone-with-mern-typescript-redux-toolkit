45bfee8fdd04c3179cd1d2037b70388b78667447
MD Shimul <mdshemul480@gmail.com>
2021-08-17 16:09:25 +0600

    adding code for modal

--- client/src/pages/Home/components/PostCreate.tsx
+++ client/src/pages/Home/components/PostCreate.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 
+import PostCreateModal from './PostCreateModal';
+
 import { UserReducer } from '../../../types/userReducer';
 const PostCreator = () => {
   const [showModal, setShowModal] = useState(false);
@@ -17,28 +19,31 @@ const PostCreator = () => {
   };
 
   return (
-    <div className="bg-light rounded shadow-sm">
-      <div className="d-flex align-items-center p-2">
-        {user && (
-          <img
-            width="50px"
-            height="50px"
-            className="rounded-circle "
-            src={
-              process.env.REACT_APP_BACKEND_API_LINK +
-              `/public/${user.profileImage}`
-            }
-            alt=""
-          />
-        )}
-        <p
-          className="flex-fill px-2 py-2 mx-2 mt-3 post_create_section"
-          onClick={showModalHandler}
-        >
-          what's on your mind, {user?.firstName}.?
-        </p>
+    <>
+      <PostCreateModal show={showModal} onHide={closeModalHandler} />
+      <div className="bg-light rounded shadow-sm">
+        <div className="d-flex align-items-center p-2">
+          {user && (
+            <img
+              width="50px"
+              height="50px"
+              className="rounded-circle "
+              src={
+                process.env.REACT_APP_BACKEND_API_LINK +
+                `/public/${user.profileImage}`
+              }
+              alt=""
+            />
+          )}
+          <p
+            className="flex-fill px-2 py-2 mx-2 mt-3 post_create_section"
+            onClick={showModalHandler}
+          >
+            what's on your mind, {user?.firstName}.?
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- client/src/pages/Home/components/PostCreateModal.tsx
+++ client/src/pages/Home/components/PostCreateModal.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Modal } from 'react-bootstrap';
 
-const PostCreateModal = () => {
-  return <div></div>;
+const PostCreateModal: React.FC<{ show: boolean; onHide: () => void }> = (
+  props
+) => {
+  const { show, onHide } = props;
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>Create Post</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h1>hello world</h1>
+      </Modal.Body>
+    </Modal>
+  );
 };
 
 export default PostCreateModal;
