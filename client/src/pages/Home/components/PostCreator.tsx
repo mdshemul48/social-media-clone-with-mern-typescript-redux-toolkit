a3037260f616da2581f6a9e4153b5bb3cd0bcb2d
MD Shimul <mdshemul480@gmail.com>
2021-08-17 15:55:21 +0600

    adding handler for showing modal and hiding modal

--- client/src/pages/Home/components/PostCreator.tsx
+++ client/src/pages/Home/components/PostCreator.tsx
@@ -1,32 +1,46 @@
-import React from 'react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { UserReducer } from '../../../types/userReducer';
 const PostCreator = () => {
+  const [showModal, setShowModal] = useState(false);
+
   const { user } = useSelector(
     (state: { userState: UserReducer }) => state.userState
   );
 
+  const showModalHandler = () => {
+    setShowModal(true);
+  };
+  const closeModalHandler = () => {
+    setShowModal(false);
+  };
+
   return (
-    <div className="bg-light rounded shadow-sm">
-      <div className="d-flex align-items-center p-2">
-        {user && (
-          <img
-            width="50px"
-            height="50px"
-            className="rounded-circle "
-            src={
-              process.env.REACT_APP_BACKEND_API_LINK +
-              `/public/${user.profileImage}`
-            }
-            alt=""
-          />
-        )}
-        <p className="flex-fill px-2 py-2 mx-2 mt-3 post_create_section">
-          what's on your mind, {user?.firstName}.?
-        </p>
+    <>
+      <div className="bg-light rounded shadow-sm">
+        <div className="d-flex align-items-center p-2">
+          {user && (
+            <img
+              width="50px"
+              height="50px"
+              className="rounded-circle "
+              src={
+                process.env.REACT_APP_BACKEND_API_LINK +
+                `/public/${user.profileImage}`
+              }
+              alt=""
+            />
+          )}
+          <p
+            className="flex-fill px-2 py-2 mx-2 mt-3 post_create_section"
+            onClick={showModalHandler}
+          >
+            what's on your mind, {user?.firstName}.?
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
