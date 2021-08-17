a401c2cdd7e99072800eb9ad850352ed5c83a6f2
MD Shimul <mdshemul480@gmail.com>
2021-08-17 19:36:18 +0600

    adding postbox for adding post

--- client/src/pages/Home/components/PostForm.tsx
+++ client/src/pages/Home/components/PostForm.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+
+import { UserReducer } from '../../../types/userReducer';
+
 const PostForm = () => {
+  const { user } = useSelector(
+    (state: { userState: UserReducer }) => state.userState
+  );
   return (
     <Form>
       <Form.Group>
         <Form.Control
           as="textarea"
           rows={3}
-          placeholder={`What's on your mind, ${'gg'}`}
-        ></Form.Control>
+          placeholder={`What's on your mind, ${user?.firstName}`}
+        />
       </Form.Group>
     </Form>
   );
