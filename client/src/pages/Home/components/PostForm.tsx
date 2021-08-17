c36a5961ee1d807ff4c912cf1fcff329d6f20196
MD Shimul <mdshemul480@gmail.com>
2021-08-17 20:01:41 +0600

    adding style for post modal

--- client/src/pages/Home/components/PostForm.tsx
+++ client/src/pages/Home/components/PostForm.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 
 import { UserReducer } from '../../../types/userReducer';
 
+import PostModalProfile from './PostModalProfile';
 const PostForm = () => {
   const { user } = useSelector(
     (state: { userState: UserReducer }) => state.userState
   );
   return (
-    <Form>
-      <Form.Group>
-        <Form.Control
-          as="textarea"
-          rows={3}
-          placeholder={`What's on your mind, ${user?.firstName}`}
-        />
-      </Form.Group>
-    </Form>
+    <>
+      <PostModalProfile />
+      <Form>
+        <Form.Group>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder={`What's on your mind, ${user?.firstName}`}
+          />
+        </Form.Group>
+      </Form>
+    </>
   );
 };
 
