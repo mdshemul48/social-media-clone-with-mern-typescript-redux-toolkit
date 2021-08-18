7bb1f59d4361661486263100a037e68c1e574c47
MD Shimul <mdshemul480@gmail.com>
2021-08-18 22:38:56 +0600

    adding errors to postMethod

--- client/src/store/asyncMethods/postMethod.ts
+++ client/src/store/asyncMethods/postMethod.ts
@@ -4,7 +4,7 @@ import { stateInterface } from '../../types/stateInterface';
 import { post } from '../../types/postInterface';
 import { AppDispatch } from '../index';
 
-import { setPost } from '../reducers/postReducer';
+import { setPost, setErrors } from '../reducers/postReducer';
 
 export const createPost = (formState: FormData) => {
   return async (dispatch: AppDispatch, getState: () => stateInterface) => {
@@ -25,9 +25,9 @@ export const createPost = (formState: FormData) => {
         config
       );
       dispatch(setPost(data.post));
-      console.log(data.post);
-    } catch (error) {
-      console.log(error);
+    } catch (error: any) {
+      const { errors } = error?.response;
+      dispatch(setErrors(errors));
     }
   };
 };
