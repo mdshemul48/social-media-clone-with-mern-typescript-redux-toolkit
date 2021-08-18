5a3f87de17f15404a271cf802a851704239510f7
MD Shimul <mdshemul480@gmail.com>
2021-08-18 13:44:59 +0600

    adding post method for creating post

--- client/src/store/asyncMethods/postMethod.ts
+++ client/src/store/asyncMethods/postMethod.ts
@@ -1,7 +1,33 @@
+import axios from '../../config/axios';
+
 import { postForm } from '../../types/postInterface';
+import { stateInterface } from '../../types/stateInterface';
+import { post } from '../../types/postInterface';
 import { AppDispatch } from '../index';
+
 import { setPost } from '../reducers/postReducer';
 
 export const createPost = (formState: postForm) => {
-  return async (dispatch: AppDispatch, state) => {};
+  return async (dispatch: AppDispatch, state: stateInterface) => {
+    const {
+      userState: { token }
+    } = state;
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    };
+
+    try {
+      const { data }: { data: { msg: string; post: post } } = await axios.post(
+        '/post',
+        formState,
+        config
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
--- client/src/types/stateInterface.ts
+++ client/src/types/stateInterface.ts
@@ -1,6 +1,6 @@
 import { postsState } from './postInterface';
 import { UserReducer } from './userReducer';
-export interface state {
+export interface stateInterface {
   postsState: postsState;
   userState: UserReducer;
 }
