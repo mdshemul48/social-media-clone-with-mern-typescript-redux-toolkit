3b7a165c1b4afd37f9f4968a9574bb9ef5f78185
MD Shimul <mdshemul480@gmail.com>
2021-09-07 18:58:53 +0600

    adding left side for home page

--- client/src/pages/Home/components/LeftSide.tsx
+++ client/src/pages/Home/components/LeftSide.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
-
+import SideBarRow from './SideBarRow';
 const LeftSide = () => {
-  return <div>
-      
-      
-  </div>;
+  return (
+    <div>
+      <SideBarRow
+        imagePath={
+          'http://localhost:5000/public/fdc49ddc-7d1c-4061-a4b1-3d55ed10b29d.jpeg'
+        }
+        text="MD. Shimul"
+      />
+      <SideBarRow
+        imagePath={
+          'https://static.xx.fbcdn.net/rsrc.php/v3/y8/r/S0U5ECzYUSu.png'
+        }
+        text="Friends"
+      />
+      <SideBarRow
+        imagePath={
+          'https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/PrjLkDYpYbH.png'
+        }
+        text="Groups"
+      />
+      <SideBarRow
+        imagePath={
+          'https://static.xx.fbcdn.net/rsrc.php/v3/yH/r/kyCAf2jbZvF.png'
+        }
+        text="Pages"
+      />
+      <SideBarRow
+        imagePath={
+          'https://static.xx.fbcdn.net/rsrc.php/v3/yU/r/D2y-jJ2C_hO.png'
+        }
+        text="Marketplace"
+      />
+      <SideBarRow
+        imagePath={
+          'https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/duk32h44Y31.png'
+        }
+        text="Watch"
+      />
+    </div>
+  );
 };
 
 export default LeftSide;
--- client/src/pages/Home/components/sideBarRow.tsx
+++ client/src/pages/Home/components/sideBarRow.tsx
@@ -3,11 +3,19 @@ import React from 'react';
 const sideBarRow: React.FC<{ imagePath: string; text: string }> = (props) => {
   const { imagePath, text } = props;
   return (
-    <div className="d-flex align-items-center">
+    <div className="d-flex align-items-center my-2">
       <div className="">
-        <img src={imagePath} alt="sidebar" />
+        <img
+          width="35px"
+          height="35px"
+          className="rounded-circle"
+          src={imagePath}
+          alt="sidebar"
+        />
       </div>
-      <div>{text}</div>
+      <div className="ms-2">
+        <span className="fs-6 sidebar-text">{text}</span>
+      </div>
     </div>
   );
 };
