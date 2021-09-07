103af94fcbdb7e0698c42ae1e3539785b41b45eb
MD Shimul <mdshemul480@gmail.com>
2021-09-07 18:40:13 +0600

    adding image and text components for showing data

--- client/src/pages/Home/components/sideBarRow.tsx
+++ client/src/pages/Home/components/sideBarRow.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const sideBarRow = () => {
-  return <div></div>;
+const sideBarRow: React.FC<{ imagePath: string; text: string }> = (props) => {
+  const { imagePath, text } = props;
+  return (
+    <div className="d-flex align-items-center">
+      <div className="">
+        <img src={imagePath} alt="sidebar" />
+      </div>
+      <div>{text}</div>
+    </div>
+  );
 };
 
 export default sideBarRow;
